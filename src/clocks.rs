1546ae2e5ab78d46e3cb4d0920a03476a07af6ac
Rory Coffey <coffeyrt@gmail.com>
2021-08-14 16:45:01 -0400

    chaged to crate::

--- src/clocks.rs
+++ src/clocks.rs
@@ -1,9 +1,9 @@
-use mbta_countdown;
 use chrono;
 use chrono::prelude::*;
-struct Clocks {
-    display_ht16k33: Option<mbta_countdown::ht16k33::ClockDisplay>,
-    display_tm1637: Option<mbta_countdown::tm1637::ClockDisplay>,
+
+pub struct Clocks {
+    display_ht16k33: Option<crate::ht16k33::ClockDisplay>,
+    display_tm1637: Option<crate::tm1637::ClockDisplay>,
 }
 
 impl Clocks {
@@ -13,7 +13,7 @@ impl Clocks {
         address: Option<u8>,
     ) -> Result<Clocks, Box<dyn std::error::Error>> {
         if clock_type == "HT16K33".to_string() {
-            let clock_ht16k33 = mbta_countdown::ht16k33::ClockDisplay::new(
+            let clock_ht16k33 = crate::ht16k33::ClockDisplay::new(
                 address.unwrap(),
                 clock_brightness,
             )?;
@@ -22,7 +22,7 @@ impl Clocks {
                 display_tm1637: None,
             })
         } else {
-            let clock_tm1637 = mbta_countdown::tm1637::ClockDisplay::new(clock_brightness)?;
+            let clock_tm1637 = crate::tm1637::ClockDisplay::new(clock_brightness)?;
             Ok(Clocks {
                 display_ht16k33: None,
                 display_tm1637: Some(clock_tm1637),
